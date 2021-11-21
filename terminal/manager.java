fcae250d86110556d1a66c1561c6cb48c8277615
unknown <betruong@pdx.edu>
2021-11-21 08:35:48 -0800

    making terminal package

---
+++ terminal/manager.java
@@ -0,0 +1,7 @@
+package terminal;
+public class manager {
+    public void manager_main() {
+        System.out.println("You are in the Manager's Terminal");
+        return;
+    }
+}
---
+++ terminal/provider.java
@@ -0,0 +1,10 @@
+package terminal;
+
+public class provider {
+    
+    public void provider_main() {
+        System.out.println("You are in the Provider's Terminal");
+        return;
+    }
+
+}
