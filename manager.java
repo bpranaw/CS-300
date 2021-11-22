057d17d62349b353831b2097f50a724dfcd1677c
unknown <betruong@pdx.edu>
2021-11-22 06:43:18 -0800

    removing files from outside of terminal package

--- manager.java
+++
@@ -1,6 +0,0 @@
-public class manager {
-    public void manager_main() {
-        System.out.println("You are in the Manager's Terminal");
-        return;
-    }
-}
--- provider.java
+++
@@ -1,8 +0,0 @@
-public class provider {
-    
-    public void provider_main() {
-        System.out.println("You are in the Provider's Terminal");
-        return;
-    }
-
-}
