32498cd137c0b9785a6b400da3dd8ce057947050
kksk117 <81442650+kksk117@users.noreply.github.com>
2021-11-22 23:51:07 -0800

    Create member_data.java

---
+++ terminal/member_data.java
@@ -0,0 +1,12 @@
+package terminal;
+
+public class member_data
+{
+    int ID;
+    int zip;
+    String num;
+    String member_name;
+    String street_name;
+    String city;
+    String state;
+}
