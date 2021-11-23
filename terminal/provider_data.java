32a4e3a895bb7245b9ee21007099a31ec1e0fa76
kksk117 <81442650+kksk117@users.noreply.github.com>
2021-11-22 16:02:45 -0800

    Create provider_data.java

---
+++ terminal/provider_data.java
@@ -0,0 +1,12 @@
+package terminal;
+
+public class provider_data
+{
+    int ID;
+    int zip;
+    String num;
+    String provider_name;
+    String street_name;
+    String city;
+    String state;
+}
